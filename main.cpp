0a6f0727736bff316a7cd3699ae09e762d732534
zacharypainter@knights.ucf.edu <zacharypainter@knights.ucf.edu>
2018-12-18 19:26:34 -0500

    Line endings fixed

--- main.cpp
+++ main.cpp
@@ -11,15 +11,15 @@ QStack<int> *s;
 void work(int thread_id, int num_ops, int push_ratio)
 {
 	boost::mt19937 randomGen;
-    randomGen.seed(time(0));
-    boost::uniform_int<uint32_t> randomDist(1,1000);
+	randomGen.seed(time(0));
+	boost::uniform_int<uint32_t> randomDist(1, 1000);
 
 	for (int i = 0; i < num_ops; i++)
 	{
 		int r = randomDist(randomGen);
 		bool result;
 
-		if ((r%101) <= push_ratio)
+		if ((r % 101) <= push_ratio)
 		{
 			result = s->push(thread_id, i, r);
 		}
@@ -48,7 +48,7 @@ int main(int argc, char** argv)
 
 	for (int i = 0; i < NUM_THREADS; i++)
 	{
-		threads.push_back(std::thread(work, i, NUM_OPS/NUM_THREADS, RATIO_PUSH));
+		threads.push_back(std::thread(work, i, NUM_OPS / NUM_THREADS, RATIO_PUSH));
 	}
 
 	for (std::thread &t : threads)
--- qstack.h
+++ qstack.h
@@ -11,38 +11,38 @@
 template<typename T>
 class QStack
 {
-	public:
-		class Node;
+public:
+	class Node;
 
-		QStack(int num_threads, int num_ops) : 
+	QStack(int num_threads, int num_ops) :
 		data(num_threads),
 		num_threads(num_threads)
+	{
+		top = new std::atomic<Node*>[num_threads];
+		for (int i = 0; i < num_threads; i++)
 		{
-			top = new std::atomic<Node*>[num_threads];
-			for (int i = 0; i < num_threads; i++) 
-			{
-        		top[i] = nullptr;
-      		}
-
-      		for (auto &v : data)
-      		{
-      			for (int i = 0; i < num_ops/num_threads; i++)
-      			{
-      				v.push_back(new Node());
-      			}
-      		}
+			top[i] = nullptr;
 		}
 
-		bool push(int tid, int opn, T v);
+		for (auto &v : data)
+		{
+			for (int i = 0; i < num_ops / num_threads; i++)
+			{
+				v.push_back(new Node());
+			}
+		}
+	}
 
-    	bool pop(int tid, int opn, T &v);
+	bool push(int tid, int opn, T v);
 
-    	void dumpNodes(std::ofstream &p);
+	bool pop(int tid, int opn, T &v);
 
-	private:
-    	std::atomic<Node *> *top; // node pointer array for branches
-    	std::vector<std::vector<Node *>> data; //Array for pre-allocated data
-    	int num_threads;
+	void dumpNodes(std::ofstream &p);
+
+private:
+	std::atomic<Node *> *top; // node pointer array for branches
+	std::vector<std::vector<Node *>> data; //Array for pre-allocated data
+	int num_threads;
 };
 
 template<typename T>
@@ -59,78 +59,78 @@ bool QStack<T>::push(int tid, int opn, T v)
 	int loop = 0;
 	int headIndex = 0;
 
-	while (true) 
+	while (true)
 	{
 		//Read top of stack
-	    Node *cur = top[headIndex].load();
-	    elem->next(cur);
-	    elem->level(headIndex);
+		Node *cur = top[headIndex].load();
+		elem->next(cur);
+		elem->level(headIndex);
 
-    	//Attemp to update main branch
-    	if (top[headIndex].compare_exchange_weak(cur, elem)) 
-    	{
-      		if (cur != nullptr) {
-        		// Lazily set the pred pointer back to elem, the new node
-        		cur->pred(elem);
-        		//fprintf(stdout, "tid=%02i-%05i-%03i push STAC elem=%p elem->next()=%p, cur=%p cur->next()=%p cur->pred()=%p cur->value()=%c \n", \
-        		//                 tid,    opn,  loop,     elem,   elem->next(),    cur,   cur->next(),   cur->pred(),   cur->value() );
-        	}
-        	return true;
-    	}
+		//Attemp to update main branch
+		if (top[headIndex].compare_exchange_weak(cur, elem))
+		{
+			if (cur != nullptr) {
+				// Lazily set the pred pointer back to elem, the new node
+				cur->pred(elem);
+				//fprintf(stdout, "tid=%02i-%05i-%03i push STAC elem=%p elem->next()=%p, cur=%p cur->next()=%p cur->pred()=%p cur->value()=%c \n", \
+				//                 tid,    opn,  loop,     elem,   elem->next(),    cur,   cur->next(),   cur->pred(),   cur->value() );
+			}
+			return true;
+		}
 
-    	// If we see a lot of contention try another branch
-    	if ((cur != nullptr) && loop > CAS_LIMIT) 
-    	{
-    		headIndex = (headIndex + 1) % this->num_threads;
-    	}  
-	    ++loop;
+		// If we see a lot of contention try another branch
+		if ((cur != nullptr) && loop > CAS_LIMIT)
+		{
+			headIndex = (headIndex + 1) % this->num_threads;
+		}
+		++loop;
 	}
 }
 
 template<typename T>
-bool QStack<T>::pop(int tid, int opn, T& v) 
+bool QStack<T>::pop(int tid, int opn, T& v)
 {
-  	int loop = 0;
-  	int headIndex = 0;
+	int loop = 0;
+	int headIndex = 0;
 
-  	while (true) 
-  	{
-    	Node *cur = top[headIndex].load();
-    	Node *next = nullptr;
+	while (true)
+	{
+		Node *cur = top[headIndex].load();
+		Node *next = nullptr;
 
-    	if (cur != nullptr) 
-    	{
-    		//If the next node is part of our currrent branch, make it the head of this branch
-    		//Otherwise, we want the head of the current branch to become null
-    		if (cur->next()->level() == cur->level())
-    			next = cur->next();
-    	} 
-    	else 
-    	{
-      		//If curr does not exist, the main branch is empty. Push to sub-tree
-      		v = int('?');
-      		return false;
-    	}
+		if (cur != nullptr)
+		{
+			//If the next node is part of our currrent branch, make it the head of this branch
+			//Otherwise, we want the head of the current branch to become null
+			if (cur->next()->level() == cur->level())
+				next = cur->next();
+		}
+		else
+		{
+			//If curr does not exist, the main branch is empty. Push to sub-tree
+			v = int('?');
+			return false;
+		}
 
-    	//Pop from selected head
-    	if (top[headIndex].compare_exchange_weak(cur, next)) 
-    	{ 
-      		v = cur->value();
-      		//delete cur;
-      		return true;
-    	}
+		//Pop from selected head
+		if (top[headIndex].compare_exchange_weak(cur, next))
+		{
+			v = cur->value();
+			//delete cur;
+			return true;
+		}
 
-    	// If we see a lot of contention try another branch
-    	if (loop > CAS_LIMIT) 
-    	{
-    		headIndex = (headIndex + 1) % this->num_threads;
-    	}  
-  		++loop;
-  	}
+		// If we see a lot of contention try another branch
+		if (loop > CAS_LIMIT)
+		{
+			headIndex = (headIndex + 1) % this->num_threads;
+		}
+		++loop;
+	}
 }
 
 template<typename T>
-void QStack<T>::dumpNodes(std::ofstream &p) 
+void QStack<T>::dumpNodes(std::ofstream &p)
 {
 	for (int i = 0; i < this->num_threads; i++)
 	{
@@ -152,25 +152,25 @@ void QStack<T>::dumpNodes(std::ofstream &p)
 template<typename T>
 class QStack<T>::Node
 {
-	public:
-		Node (T &v) : _val(v) {};
-		Node () {};
+public:
+	Node (T &v) : _val(v) {};
+	Node () {};
 
-		T value() { return _val; };
-		void value(T &v) { _val = v; };
+	T value() { return _val; };
+	void value(T &v) { _val = v; };
 
-		void next(Node *n) {_next = n; };
-		Node *next() { return _next; };
+	void next(Node *n) {_next = n; };
+	Node *next() { return _next; };
 
-		void pred(Node *p) { _pred = p; };
-		Node *pred() { return _pred; };
+	void pred(Node *p) { _pred = p; };
+	Node *pred() { return _pred; };
 
-		void level(int i) { _branch_level = i; };
-		int level() { return _branch_level; };
+	void level(int i) { _branch_level = i; };
+	int level() { return _branch_level; };
 
-	private:
-		T _val {NULL};
-		Node *_next {nullptr};
-		Node *_pred {nullptr};
-		int _branch_level;
+private:
+	T _val {NULL};
+	Node *_next {nullptr};
+	Node *_pred {nullptr};
+	int _branch_level;
 };
