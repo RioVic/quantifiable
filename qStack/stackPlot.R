11ac573f2b311b564a01591d1d861f03e8bd8f32
Victor Cook <victorjcook@gmail.com>
2019-02-25 06:00:22 -0500

    PODC 18 Feb

--- qStack/stackPlot.R
+++ qStack/stackPlot.R
@@ -6,7 +6,7 @@ setwd('/Users/victor/Downloads/resultsQ/StackAMD')
 
 cbbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
-sQ <- read.table('StackAMD50.dat', sep = "",header = TRUE)
+sQ <- read.table('StackAMD.dat', sep = "",header = TRUE)
 # type	mix	threads	ms	ops
 sQ <- sQ %>% mutate(architecture=as.factor(arch), algorithm=as.factor(type), 
                     mix=as.factor(mix), threads=as.numeric(threads), 
@@ -24,32 +24,34 @@ sQ
 cbbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442") # , "#009E73", "#0072B2", "#D55E00", "#CC79A7")
 
 p3 <- ggplot(data=sQ, aes(x=xOffset, y=op_per_ms, color=algorithm, 
-                                  linetype=algorithm)) +  #shape=mix
-  geom_smooth(level=0.95) +
-  geom_point(alpha=0.70, shape=3, size=1) +
+                                  linetype=algorithm, shape=mix)) +  #shape=mix
+  geom_smooth(level=0.95, span = 0.33, size=0.5) +
+  # stat_smooth(method = "loess", formula = y ~ x, size = 1) +
+  geom_point(alpha=0.75, size=1) + # shape=3,
   scale_color_manual(values=cbbPalette) +
   # theme(legend.position="top") +
   scale_linetype_manual(values=c( "dashed", "solid", "dotted", "twodash")) +
-  scale_shape_manual(values=c(1, 3, 4)) +
+  scale_shape_manual(values=c(1, 3, 4)) + 
   scale_x_continuous(expand = c(0, 0), limits = c(0.5, 64.5), 
                      minor_breaks = seq(0.5,64.5,1.0)) + 
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 57),
-                     minor_breaks = seq(5,67,5)) +
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 87),
+                     minor_breaks = seq(5,87,5)) +
   xlab("Threads") +
   ylab(expression("Method calls / "~mu~"-sec")) + theme_bw() +
-  theme(axis.title=element_text(size=14), 
-        axis.text=element_text(size=12),
-        panel.grid.major = element_blank(),
+  theme(axis.title=element_text(size=18), 
+        axis.text=element_text(size=14),
+        panel.grid.major.x = element_blank(),
         panel.grid.minor = element_line(size = 0.5),
-        legend.text=element_text(size=14),
-        legend.spacing.x = unit(0.1, 'cm'),
+        legend.text=element_text(size=18),
+        #legend.spacing.x = unit(0.1, 'cm'),
         legend.position="top",
         legend.title = element_blank(),
      #   legend.key = element_rect(fill = 'white', size = 2.5),
       #  legend.key.fill = 'white',
         legend.key = element_rect(fill = "white"),
         legend.key.width = unit(1, "cm")) +
-   guides(color=guide_legend(override.aes=list(fill=NA))) + 
+   guides(color=guide_legend(override.aes=list(fill=NA))) +
+  guides(shape = guide_legend(override.aes = list(size = 3)))
   guides(fill=guide_legend(nrow=2,byrow=TRUE))
 p3
-ggsave(paste("StackAMD50",".png", sep = ""), p3, "png")
+ggsave(paste("StackAMDH",".png", sep = ""), p3, "png")
