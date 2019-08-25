1921576185c66a1cd423ad38cf814e5c2a407ef1
Zachary Painter <zacharypainter@knights.ucf.edu>
2019-08-25 15:33:48 -0400

    Added 1 extra case to bulk.sh

--- qStack/bulk.sh
+++ qStack/bulk.sh
@@ -5,25 +5,25 @@ Stack="$1"
 
 for i in {1..32}
 do
-	$Program i 100000 1 $Stack
+	$Program $i 100000 1 $Stack
 done
 
 for i in {1..32}
 do
-	$Program i 10000 10 $Stack
+	$Program $i 10000 10 $Stack
 done
 
 for i in {1..32}
 do
-	$Program i 1000 100 $Stack
+	$Program $i 1000 100 $Stack
 done
 
 for i in {1..32}
 do
-	$Program i 100 1000 $Stack
+	$Program $i 100 1000 $Stack
 done
 
 for i in {1..32}
 do
-	$Program i 10 10000 $Stack
+	$Program $i 10 10000 $Stack
 done
