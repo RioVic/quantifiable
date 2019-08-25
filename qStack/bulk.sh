2aa9d414161e06b633614a6d3a06e858806fbaac
Zachary Painter <zacharypainter@knights.ucf.edu>
2019-08-25 15:24:07 -0400

    bulk.sh improved for testing

--- qStack/bulk.sh
+++ qStack/bulk.sh
@@ -3,17 +3,22 @@
 Program="./a.out"
 Stack="$1"
 
-$Program 4 1000000 1 $Stack
-$Program 8 1000000 1 $Stack
-$Program 16 1000000 1 $Stack
-$Program 32 1000000 1 $Stack
+for i in {1..32}
+do
+	$Program i 100000 1 $Stack
+done
 
-$Program 4 100000 10 $Stack
-$Program 8 100000 10 $Stack
-$Program 16 100000 10 $Stack
-$Program 32 100000 10 $Stack
+for i in {1..32}
+do
+	$Program i 10000 10 $Stack
+done
 
-$Program 4 10000 100 $Stack
-$Program 8 10000 100 $Stack
-$Program 16 10000 100 $Stack
-$Program 32 10000 100 $Stack
+for i in {1..32}
+do
+	$Program i 1000 100 $Stack
+done
+
+for i in {1..32}
+do
+	$Program i 10 10000 $Stack
+done
